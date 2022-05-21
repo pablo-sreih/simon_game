8fa8b723527c8c6dc2112db8d418377ca7839d25
Pablo Sreih <88791435+pablo-sreih@users.noreply.github.com>
2022-05-21 11:15:29 +0300

    Added Sounds to Buttons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,10 +3,18 @@ var red_button = document.getElementById("red");
 var yellow_button = document.getElementById("yellow");
 var blue_button = document.getElementById("blue");
 
-green_button.onclick = function(){animateOnClick(green_button)};
-red_button.onclick = function(){animateOnClick(red_button)};
-yellow_button.onclick = function(){animateOnClick(yellow_button)};
-blue_button.onclick = function(){animateOnClick(blue_button)};
+
+var green_sound = new Audio("assets/sounds/green.mp3");
+var red_sound = new Audio("assets/sounds/red.mp3");
+var yellow_sound = new Audio("assets/sounds/yellow.mp3");
+var blue_sound = new Audio("assets/sounds/blue.mp3");
+var wrong_sound = new Audio("assets/sounds/wrong.mp3");
+
+
+green_button.onclick = function(){animateOnClick(green_button), green_sound.play()};
+red_button.onclick = function(){animateOnClick(red_button), red_sound.play()};
+yellow_button.onclick = function(){animateOnClick(yellow_button), yellow_sound.play()};
+blue_button.onclick = function(){animateOnClick(blue_button), blue_sound.play()};
 
 
 function animateOnClick(x){
@@ -16,5 +24,5 @@ function animateOnClick(x){
     setTimeout(function(){
         x.style.backgroundColor = init_color;
         x.style.boxShadow = 'none';
-    }, 100);
+    }, 200);
 }
